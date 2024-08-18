9c80fc3230dcf6b87f2a8de625104004e2f14e49
Tony Toon <tony@deepthaw.net>
2024-08-17 19:41:31 -0500

    switch to gruvbox-baby for tree-sitter support.

--- lua/plugins/gruvbox-baby.lua
+++ lua/plugins/gruvbox-baby.lua
@@ -1,8 +1,8 @@
-return { "ellisonleao/gruvbox.nvim", 
+return { "luisiacc/gruvbox-baby",
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-      vim.cmd [[ colorscheme gruvbox ]]
+      vim.cmd [[ colorscheme gruvbox-baby ]]
     end
     }
 
